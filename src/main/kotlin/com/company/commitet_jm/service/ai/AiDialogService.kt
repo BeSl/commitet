f727c2033962f698be7c3a028691d82644692cd3
BeSl <belyakovva@me.com>
2025-06-08 23:30:24 +0300

    добавил в чат память

--- src/main/kotlin/com/company/commitet_jm/service/ai/AiDialogService.kt
+++ src/main/kotlin/com/company/commitet_jm/service/ai/AiDialogService.kt
@@ -1,51 +1,84 @@
 package com.company.commitet_jm.service.ai
 
+
+import com.company.commitet_jm.entity.ChatSession
 import com.company.commitet_jm.service.ChatHistoryService
 import io.jmix.core.DataManager
 import io.jmix.flowui.UiEventPublisher
 import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor
+import org.springframework.ai.chat.memory.ChatMemory
+import org.springframework.ai.chat.memory.MessageWindowChatMemory
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository
+import org.springframework.ai.chat.messages.UserMessage
 import org.springframework.ai.chat.model.ChatModel
-
-
+import org.springframework.ai.chat.prompt.ChatOptions
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 
 @Component
-class AiDialogService(private val dataManager: DataManager,
-                      private val uiEventPublisher: UiEventPublisher,
-                      private val chatModel: ChatModel) {
+class AiDialogService(
+    private val dataManager: DataManager,
+    private val uiEventPublisher: UiEventPublisher,
+    private val chatModel: ChatModel
+) {
 
-
-    fun aiAnswerMessage(){
+  fun aiAnswerMessage(){
         val history = ChatHistoryService(dataManager,uiEventPublisher)
         val req = history.messageToResponse()
 //        val msg = getQueueMessage(history)
+        if (req==null){
+            return
+        }
 
         req?.content = "DONE!!!"
+        val id_chat = req.session!!.user!!.getUsername()
 
-        if (req != null) {
-            val chatClient: ChatClient = ChatClient.builder(chatModel)
+        val chatMemory = loadMemory(req.session, id_chat!!)
+
+
+//        chatMemory.add(id_chat, UserMessage("ddsdsd"))
+
+        val chatClient: ChatClient = ChatClient.builder(chatModel)
+            .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .build()
-//
-            req.content = req.parrentMessage!!.content?.let {
-                chatClient.prompt("ты помощник разработчика отвечай в рамках проекта") // Set advisor parameters at runtime
-//                    .advisors { advisor ->
-//                        advisor.param("chat_memory_conversation_id", "678")
-//                            .param("chat_memory_response_size", 100)
-//                    }
 
-                    .user(it)
-//                    .options(ChatOptions.builder()
-//                        .temperature(0.1)  // Very deterministic output
-//                        .build())
+        req.content =
+                chatClient.prompt("ты помощник разработчика отвечай на вопросы программирования") // Set advisor parameters at runtime
+                    .advisors { advisor ->
+                        advisor
+                            .param(ChatMemory.CONVERSATION_ID, id_chat)
+                            .param("chat_memory_response_size", 5)
+                    }
+                    .user(req.parrentMessage!!.content)
+                    .options(
+                        ChatOptions.builder()
+                        .temperature(0.5)  // Very deterministic output
+                        .build())
                     .call()
                     .content()?.toString()
-            } ?:"no connection"
 
             req.generated = true
             history.saveResponse(req)
         }
 
+    private fun loadMemory(ses: ChatSession?, id:String): ChatMemory {
+        var chatM: ChatMemory = MessageWindowChatMemory.builder()
+            .build()
+
+        val history = ChatHistoryService(dataManager,uiEventPublisher)
+        if (ses!=null) {
+            val msg = history.getHistory(ses)
+            for (ms in msg) {
+                chatM.add(id, UserMessage(ms.content))
+            }
+        }
+
+        return chatM
     }
+
+
+
 
 }
